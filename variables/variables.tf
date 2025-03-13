a06121867916f111fe7a05f08a6b4fe0dd6c76a3
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-03-13 12:49:00 -0400

    variables file

---
+++ variables/variables.tf
@@ -0,0 +1,44 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is RHEL9 AMI ID"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type        = map
+  default     = {
+    Project = "expense"
+    Component ="backend"
+    Encironment = "dev"
+    Name = "expense-backend-dev"
+  }
+  description = "description"
+}
+
+variable "from_port" {
+  type        = number
+  default     = 22
+}
+
+variable "to_port" {
+  type        = number
+  default     = 22
+}
+
+variable "cide_blocks" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+
+variable "sg_tags" {
+  type        = map
+  default     = {
+    Name = "expense-backend-dev"
+  }
+}
